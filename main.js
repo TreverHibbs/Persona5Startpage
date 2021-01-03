1703f48dfaede95938a4e431c8fef21fcb49ffe5
TreverHibbs <treverhibbs@gmail.com>
2021-01-03 13:25:52 -0800

    Add animation

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-anime({
-  targets: '.my-rect',
-  translateX: 250
+// Find your root SVG element
+const mainSvg = document.getElementById("main-svg");
+
+// Create an SVGPoint for future math
+const pt = mainSvg.createSVGPoint();
+
+// Get point in global SVG space
+function cursorPoint(evt){
+  pt.x = evt.clientX; pt.y = evt.clientY;
+  return pt.matrixTransform(mainSvg.getScreenCTM().inverse());
+}
+
+//mainSvg.addEventListener('mousemove',function(evt){
+//  const loc = cursorPoint(evt);
+//  console.debug("mouse x coord: ", loc.x);
+//  console.debug("mouse y coord: ", loc.y);
+//},false);
+
+//animate tsukishima
+const imageTsukishima = document.getElementById("tsukishima");
+
+imageTsukishima.addEventListener('mouseover', (evt) => {
+  anime({
+    targets: imageTsukishima,
+    scale: 0.9
+  });
+  return;
 });
+
+imageTsukishima.addEventListener('mouseout', (evt) => {
+  anime({
+    targets: imageTsukishima,
+    scale: 1
+  });
+  return;
+});
+
