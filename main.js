2f1ae6bce97014df5518fb58291a4204e8af2b9a
TreverHibbs <treverhibbs@gmail.com>
2021-01-13 11:53:01 -0800

    Add onload event

--- main.js
+++ main.js
@@ -11,6 +11,14 @@ import { userInterface } from './lib/userInterface.js';
 //svg namespace
 const svgNs = 'http://www.w3.org/2000/svg' 
 
+// only display the ui when the page is fully loaded
+window.onload = (e) => {
+  console.debug("onload");
+  const gridContainer = document.querySelector('.grid-container');
+  gridContainer.classList.remove('hidden');
+  const mapImage = document.querySelector('.map-image');
+  mapImage.classList.remove('hidden');
+}
 
 //initialize interface
 userInterface();
