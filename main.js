a7c1108001d97095928bd7ff3abe019f694bae38
TreverHibbs <treverhibbs@gmail.com>
2021-09-04 11:17:39 -0700

    format code

--- main.js
+++ main.js
@@ -8,11 +8,8 @@ import { userInterface } from './lib/userInterface.js';
 // get coordinates tool for development
 //getCoordinates();
 
-//svg namespace
-const svgNs = 'http://www.w3.org/2000/svg' 
-
 // only display the ui when the page is fully loaded
-window.onload = (e) => {
+window.onload = (_) => {
   console.debug("onload");
   const gridContainer = document.querySelector('.grid-container');
   gridContainer.classList.remove('hidden');
