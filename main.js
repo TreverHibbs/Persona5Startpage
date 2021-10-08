d70ba8b4af1d060cff71710d90a33d8906950946
TreverHibbs <treverhibbs@gmail.com>
2021-10-08 10:21:20 -0700

    add slash search focus shortcut

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-import { getCoordinates } from "./lib/getCoordinate.js";
+//import { getCoordinates } from "./lib/getCoordinate.js";
 import { userInterface } from "./lib/userInterface.js";
 import { animateSearch } from "./lib/animateSearch.js";
 
 // get coordinates tool for development
-getCoordinates();
+//getCoordinates();
 
 // only display the ui when the page is fully loaded
 window.onload = (_) => {
   //start search animation
   animateSearch();
+
+  //make search shortcut listener
+  function logKey(e) {
+    if (e.code == "Slash") {
+      e.preventDefault();
+      document.getElementById("search-input").focus();
+    }
+  }
+  document.addEventListener("keydown", logKey);
 
   const gridContainer = document.querySelector(".grid-container");
   gridContainer.classList.remove("hidden");
